417d5a1001c17fbe1dad4d1cb7ccb6619f530a76
victorbzrra <victorbzrra@alu.ufc.br>
2023-01-02 21:04:09 -0300

    Start commit

---
+++ src/test/java/com/spring/auth/angular/AngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.auth.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
